9561a5cfb248a353cb64314fb552cfc484b685fa
Jeanette Payne <jeanetterpayne@gmail.com>
2020-07-11 19:35:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ def oxford_comma(array)
 if array.lenghth == 1 
   array.join
  end
-elsif array.length == 2 
+if array.length == 2 
   array.join(" and ")
  end
-elsif array.length > 2
+if array.length > 2
   array.join(", ")
   array.insert(-2, " and ")
  end
